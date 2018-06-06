b9a628b311ca28ccff091dba28191b355edc8d7c
gscigliotto <gscigliotto@gmail.com>
2018-06-06 13:04:38 -0300

    mvc

--- CerveAPP/src/edu/cerveapp/controller/IndexController.java
+++ CerveAPP/src/edu/cerveapp/controller/IndexController.java
@@ -2,18 +2,23 @@ package edu.cerveapp.controller;
 
 import edu.cerveapp.model.IGestion;
 
-public class indexController implements IGestion{
+public class IndexController implements IGestion {
 
 	@Override
 	public void LoginUsuario(String usuario, String password) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void ListarPedidos() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
